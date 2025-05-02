9d4d09d57e970b4c9d73453ffa77f1405b12e188
Manu <manasabasva@gmail.com>
2025-05-01 23:22:17 -0400

    r

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: './', // ✅ This allows relative paths so index.html can load scripts
   plugins: [react()],
 })
